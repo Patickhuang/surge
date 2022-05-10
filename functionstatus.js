8311ae3cd066666d9b6cdf35c959fee828337d92
Patickhuang <80010629+Patickhuang@users.noreply.github.com>
2022-05-10 16:35:28 +0800

    Update functionstatus.js

--- functionstatus.js
+++ functionstatus.js
@@ -18,7 +18,7 @@ if ($trigger == "button") {
 $done({
     title:"Surge  已运行"+startTime,
     content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
-    icon: crown.fill"checkmark.seal":"exclamationmark.triangle",
+    icon: icon_s?"checkmark.seal":"crown.fill",
    "icon-color":icon_s?"#16A951":"#FF7500"
 });
 })();
