324073021614a6609fc9634f61a9969c903ee9df
Patickhuang <80010629+Patickhuang@users.noreply.github.com>
2022-05-10 17:09:48 +0800

    Update flushDNS.js

--- flushDNS.js
+++ flushDNS.js
@@ -9,7 +9,7 @@ flushDNS = script-name=flushDNS,update-interval=600
 */
 
 !(async () => {
-    let panel = { title: "Flush DNS" },
+    let panel = { title: "服务|DNS" },
         showServer = true,
         dnsCache;
     if (typeof $argument != "undefined") {
