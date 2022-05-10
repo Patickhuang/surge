7c6f604a12e574ac4723dfdff8216b31d8d64c56
Patickhuang <80010629+Patickhuang@users.noreply.github.com>
2022-05-10 16:16:26 +0800

    Update stream-all.js

--- stream-all.js
+++ stream-all.js
@@ -22,7 +22,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
     let panel_result = {
       title: '流媒体解锁检测',
       content: '',
-      icon: 'play.tv.fill',
+      icon: 'sparkles.tv.fill',
       'icon-color': '#002FA7',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
