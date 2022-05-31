383a8de6a6aeb8a44b651b70e28b3c8bba3116e0
Patickhuang <80010629+Patickhuang@users.noreply.github.com>
2022-05-31 15:41:10 +0800

    Update stream.js

--- stream.js
+++ stream.js
@@ -35,7 +35,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
 
   ;(async () => {
     let panel_result = {
-      title: '流媒体解锁检测 🔍',
+      title: '流媒体解锁检测',
       content: '',
       icon: 'sparkles.tv.fill',
       'icon-color': '#1E90FF',
